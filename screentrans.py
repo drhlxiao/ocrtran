e5cf38488d6d712755c1285c2570c80309e3d553
Hualin Xiao <35291977+drhlxiao@users.noreply.github.com>
2024-03-18 00:23:48 +0100

    Update screentrans.py

--- screentrans.py
+++ screentrans.py
@@ -9,12 +9,12 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Text Translation")
-        self.setGeometry(100, 100, 500, 500)
+        self.setGeometry(100, 100, 500, 400)
 
         # Create text area
         self.text_area = QTextEdit(self)
         self.text_area.setReadOnly(True)
-        self.text_area.setGeometry(50, 50, 400, 450)
+        self.text_area.setGeometry(50, 50, 400, 340)
 
         # Call translate_text automatically
         self.translate_text()
@@ -24,11 +24,14 @@ class MainWindow(QMainWindow):
         text_from_clipboard = pyperclip.paste()
 
         # Translate text
-        translated_text = GoogleTranslator(source='auto', target='en').translate(text_from_clipboard)
-
+        english = GoogleTranslator(source='auto', target='en').translate(text_from_clipboard)
         # Update text in the text area
         self.text_area.clear()
-        self.text_area.insertPlainText(f"Original Text:\n{text_from_clipboard}\n\nTranslated Text:\n{translated_text}")
+
+        chinese= GoogleTranslator(source='auto', target='zh-CN').translate(text_from_clipboard)
+        divider='-'*20
+
+        self.text_area.insertPlainText(f"Original Text:\n{text_from_clipboard}\n\n{divider}\n English :\n{english} \n\n{divider}\n Chinese:\n{chinese}")
 
 if __name__ == "__main__":
     # Call the executable program "textshot"
@@ -38,3 +41,4 @@ if __name__ == "__main__":
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
